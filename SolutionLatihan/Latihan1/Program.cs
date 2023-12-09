12fb49a4b4e66052b54855720cdcab2a73ceff8d
Hendry Priyatno <hendry.priyatno@gmail.com>
2023-12-09 20:21:28 +0700

    intial

---
+++ SolutionLatihan/Latihan1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Saya sedang belajar bahasa C#");
+Console.WriteLine("Ini sangat mengagumkan");
+Console.WriteLine(2 + 3);
+Console.Write("ini contoh write aja");
+//Console.Write("cek bedanya");
+Console.WriteLine();
+Console.WriteLine(19.99);
+Console.WriteLine('B');
+Console.WriteLine(1 + 1 == 5 ? "jawaban ini benar" : "jawaban ini  salah");
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+
+
+
+
+
+
+
+Console.ReadKey();
